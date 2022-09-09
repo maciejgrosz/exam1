89c91a0edd7e6be3b4d92b5a36c7a30235eddb42
Maciej Groszyk <maciej.groszyk8@gmail.com>
2022-09-09 16:20:28 +0200

    delete files from backend container, fix static files

--- project/entrypoint.sh
+++ project/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-python manage.py migrate
+# python manage.py migrate
 # python manage.py updatecatalog
-python manage.py collectstatic
+# python manage.py collectstatic
 python manage.py runserver 0.0.0.0:8000
